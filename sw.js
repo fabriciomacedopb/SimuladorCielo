451e18d71b9e2a7962c2b5282240dc4fcf13712e
fabriciomacedopb <fabriciomacedopb@gmail.com>
2026-08-19 21:40:16 -0300

    Atualizar cache para layout PDF retrato

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'proposta-pagamentos-v2.6.0';
+const CACHE_NAME = 'proposta-pagamentos-v2.6.1';
 const CORE = [
   './', './index.html', './manifest.json',
   './css/app.css', './css/app-base.css', './css/app-components.css',
